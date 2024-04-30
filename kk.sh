30a4dfa6ac3a324e59ca8e35be1b4539a2fd7f3d
peng <peng@peng.com>
2024-04-30 12:50:03 +0800

    Add new feature

--- kk.sh
+++ kk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 跳过第一行，然后处理每一行
-tail -n +2 AS0-0-HKG.csv | while IFS=',' read -r ip port others
+tail -n +2 peng.csv | while IFS=',' read -r ip port others
 do
     # 将第一列和第二列中间加上冒号，并写入到TXT文件中
     echo "${ip}:${port}" >> youxuan.txt
